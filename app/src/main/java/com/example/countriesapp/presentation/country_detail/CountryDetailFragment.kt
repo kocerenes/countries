63e36e159488c053e30de1b1b0daca39ae64d108
kocerenes <eneskocerr41@gmail.com>
2022-06-18 23:41:26 +0300

    Required libraries are implemented, create navigation and fragments.

---
+++ app/src/main/java/com/example/countriesapp/presentation/country_detail/CountryDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.countriesapp.presentation.country_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.countriesapp.R
+
+
+class CountryDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/countriesapp/presentation/country_list/CountryListFragment.kt
@@ -0,0 +1,19 @@
+package com.example.countriesapp.presentation.country_list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.countriesapp.R
+
+
+class CountryListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
