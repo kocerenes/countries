2dcdc0053e31935ecf09b52356ec6dd0d64a19c1
kocerenes <eneskocerr41@gmail.com>
2022-06-20 16:37:25 +0300

    Complete design at screens.

--- app/src/main/java/com/example/countriesapp/presentation/country_detail/CountryDetailFragment.kt
+++ app/src/main/java/com/example/countriesapp/presentation/country_detail/CountryDetailFragment.kt
@@ -10,8 +10,27 @@ import com.example.countriesapp.R
 
 class CountryDetailFragment : Fragment() {
 
+    private var countryUuid = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            countryUuid = CountryDetailFragmentArgs.fromBundle(it).countryUuid
+        }
 
     }
 
