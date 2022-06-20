d02eb2ca47bc72fd1e1932820b93bb97a2c21bda
kocerenes <eneskocerr41@gmail.com>
2022-06-20 21:51:16 +0300

    Model is create.

---
+++ app/src/main/java/com/example/countriesapp/model/Model.kt
@@ -0,0 +1,10 @@
+package com.example.countriesapp.model
+
+data class Country(
+    val name : String?,
+    val capital : String?,
+    val region : String?,
+    val currency : String?,
+    val flag : String?,
+    val language : String?
+)
